3fa6a3d0acbc88f2c06a2746b54183ca874d11da
Jasonzhu0314 <jasonzhu_0314@163.com>
2021-07-30 17:12:28 +0800

    modified shared_mem && add device query

--- shared_mem_test.cu
+++ shared_mem_test.cu
@@ -4,7 +4,7 @@
 #include <cuda_runtime.h>
 using namespace std;
 
-// 1.函数外部定义，不能初始化，要在线程中初始化
+// 1.函数外部定义，不能初始化，要在线程中初始化，不同的block中会创建一个备份，不共享，都有shared memory
 __shared__ int shared_val1;
 
 __global__ void test_shared_val() {
@@ -12,8 +12,9 @@ __global__ void test_shared_val() {
     __shared__ int shared_val[10];
     // 3.函数内部定义变量 不能初始化，要在线程中初始化
     __shared__ int shared_val2;
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (threadIdx.x == 0) {
+    if (idx == 0) {
         shared_val1 = 10;
         shared_val2 = 20;
         shared_val[0] = 30;
@@ -21,11 +22,11 @@ __global__ void test_shared_val() {
 
     // 用于数据同步，否则会出现其他线程共享内存的数据没有赋值的现象，打印结果0
     __syncthreads();
-    printf("shared_val is %s\n",__isShared(&shared_val) ? "shared": "not shared");
-
-    printf("tid: %d, shared_val1: %d\n", threadIdx.x, shared_val1);
-    printf("tid: %d, shared_val2: %d\n", threadIdx.x, shared_val2);
-    printf("tid: %d, shared_val: %d\n",threadIdx.x, shared_val[0]);
+    printf("blockid: %d, tid: %d, shared_val is %s\n", blockIdx.x, idx, __isShared(&shared_val) ? "shared": "not shared");
+    // block 0 和 block 1打印的结果不一样
+    printf("blockid: %d, tid: %d, shared_val1: %d\n", blockIdx.x,  idx, shared_val1);
+    printf("blockid: %d, tid: %d, shared_val2: %d\n", blockIdx.x, idx, shared_val2);
+    printf("blockid: %d, tid: %d, shared_val: %d\n", blockIdx.x, idx, shared_val[0]);
     // TODO: cout 
     //cout << "shared_val:" << shared_val[0] << endl;
 }
@@ -43,11 +44,14 @@ __global__ void test_shared_val1() {
 }
 
 int main() {
+    const int block_nums = 2;
+    const int thread_nums = 2;
 
-    test_shared_val<<<1, 2>>> ();
+    test_shared_val<<<block_nums, thread_nums>>> ();
 
-    // <<<block_nums, thread_num, sharead_memory_size>>> 参数
-    test_shared_val1<<<1, 2, sizeof(int) * 5>>> ();
+    // <<<block_nums, thread_num, sharead_memory_size>>> 参数，每一个kernel调用时都创建一个sharedmemory
+    int shared_mem_size = sizeof(int) * 5;
+    test_shared_val1<<<block_nums, thread_nums, shared_mem_size>>> ();
 
     printf("test_shared_val\n");
     cudaDeviceSynchronize();

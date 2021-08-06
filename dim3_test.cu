f91db8c2219b578479ad3b760e1c04ef93b03ad6
Jasonzhu0314 <jasonzhu_0314@163.com>
2021-08-06 17:50:20 +0800

    modified dim3

--- dim3_test.cu
+++ dim3_test.cu
@@ -2,21 +2,31 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <cuda_runtime.h>
-#include "time_utils.h"
 
+__global__ void checkIndex(void)
+{
+    printf("threadIdx:(%d, %d, %d) blockIdx:(%d,%d,%d) blockDim:(%d,%d,%d) gridDim(%d,%d,%d)\n",
+        threadIdx.x, threadIdx.y, threadIdx.z,
+        blockIdx.x, blockIdx.y, blockIdx.z, blockDim.x, blockDim.y, blockDim.z,
+        gridDim.x, gridDim.y, gridDim.z);
+}
 
 int main(int argc, char* argv[]) {
-    int block_size = atoi(argv[1]);
-
-    double istart = CpuSecond();
+    
     int data_size = 32; 
-    dim3 block(block_size, 1);
-    dim3 grid((data_size - 1) / block_size + 1, 1);
+    int thread_per_block_x = 32;
+    if (argc > 2) {
+        data_size = atoi(argv[1]);
+        thread_per_block_x = atoi(argv[2]);
+    }
 
-    printf("block.x: %d, block.y: %d\n", block.x, block.y);
-    printf("grid.x: %d, grid.y: %d\n", grid.x, grid.y);
+    dim3 block(thread_per_block_x, 1);
+    dim3 grid((data_size - 1) / thread_per_block_x + 1, 1);
 
-    double ielpase = CpuSecond() - istart;
-    printf("time: %f\n", ielpase);
+    printf("block.x: %d, block.y: %d, block.z: %d\n", block.x, block.y, block.z);
+    printf("grid.x: %d, grid.y: %d, grid.z: %d\n", grid.x, grid.y, grid.z);
+    checkIndex<<<grid,block>>>();
+
+    cudaDeviceReset();
     return 0;
 }
